bfa10efe968cf88354c0f5d36a0a873d23c648ec
Enes KOC <eneskoc.0063@gmail.com>
2025-12-13 21:42:16 +0300

    seventh problem solved

---
+++ problem-7/main.py
@@ -0,0 +1,19 @@
+def isPrime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+x = 3
+counter = 2
+
+
+while counter <= 10001:
+    if isPrime(x):
+        counter += 1
+        x += 2
+    else:
+        x += 2
+
+print(x-2)  
+# 104743
