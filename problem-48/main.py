80e8df7c60c1df0d5609c9b7c76c2b67b2645829
Enes KOC <eneskoc.0063@gmail.com>
2026-01-23 22:19:37 +0300

    47th problem solved

---
+++ problem-48/main.py
@@ -0,0 +1,13 @@
+def solve():
+    total_sum = 0
+    mod = 10**10
+
+    for i in range(1, 1001):
+        total_sum += pow(i, i, mod)
+
+    result = total_sum % mod
+    print(result)
+
+if __name__ == "__main__":
+    solve()
+    # 9110846700
